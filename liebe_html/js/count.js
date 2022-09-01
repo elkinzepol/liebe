dddac2a066fce99a0fac2aa79f496f547c20e7e5
Elkin Lopez <40122667+elkinzepol@users.noreply.github.com>
2022-08-31 23:52:48 -0500

    corregido count up final

--- liebe_html/js/count.js
+++ liebe_html/js/count.js
@@ -1,35 +1,24 @@
-function updateTimer() {
-	// yyyy-MM-dd HH:mm:ss
-  past  = Date.parse("2018-10-27 08:30:00");
-  now     = new Date();
-  diff    = now - past;
+function diff() {
+  //yyyy-MM-dd HH:mm:ss
 
-  years  = Math.floor( diff / (1000*60*60*24*30*12) );
-  months = Math.floor( diff / (1000*60*60*24*30) );
-  days   = Math.floor( diff / (1000*60*60*24) );
-  hours  = Math.floor( diff / (1000*60*60) );
-  mins   = Math.floor( diff / (1000*60) );
-  secs   = Math.floor( diff / 1000 );
+  var a = moment('2018-10-27 00:00:00'); // past time
+  var b = moment(); // current time
+  diff = moment.preciseDiff(a, b, true); // objects with years, months, days, hours, minutes, seconds
 
-  y = years;
-  mo = months - years  * 12;
-  d = days   - months * 30;
-  h = hours  - days   * 24;
-  m = mins   - hours  * 60;
-  s = secs   - mins   * 60;
+  diff_years= diff['years'];
+  diff_months= diff['months'];
+  diff_days= diff['days'];
+  diff_hours= diff['hours'];
+  diff_minutes= diff['minutes'];
+  diff_seconds= diff['seconds'];
   document.getElementById("timer")
     .innerHTML =
-       '<div class="count">' + y + '<div class="displayformat">Años</div></div>' +
-       '<div class="count">' + mo + '<div class="displayformat">Meses</div></div>' +
-       '<div class="count">' + d + '<div class="displayformat">Dias</div></div>' +
-       '<div class="count">' + h + '<div class="displayformat">Horas</div></div>' +
-       '<div class="count">' + m + '<div class="displayformat">Minutos</div></div>' +
-       '<div class="count">' + s + '<div class="displayformat">Segundos</div></div>' ;
+       '<div class="count">' + diff_years + '<div class="displayformat">Años</div></div>' +
+       '<div class="count">' + diff_months + '<div class="displayformat">Meses</div></div>' +
+       '<div class="count">' + diff_days + '<div class="displayformat">Dias</div></div>' +
+       '<div class="count">' + diff_hours + '<div class="displayformat">Horas</div></div>' +
+       '<div class="count">' + diff_minutes + '<div class="displayformat">Minutos</div></div>' +
+       '<div class="count">' + diff_seconds + '<div class="displayformat">Segundos</div></div>' ;
 }
-{/* 
-<div class='count'> $1<div class='displayformat'>Days</div></div> 
-<div class='count'> $2<div class='displayformat'>Hours</div></div> 
-<div class='count'> $3<div class='displayformat'>Minutes</div></div> 
-<div class='count'> $4<div class='displayformat'>Seconds</div></div>" 
-*/}
-setInterval('updateTimer()', 1000 );
+
+setInterval(diff, 1000);
