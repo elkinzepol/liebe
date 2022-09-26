2f4eeef1e46cca3d882ea26ec7f6d4d7ba6afe04
ElkinNuevoPC <elkinluislopez@gmail.com>
2022-09-25 23:34:42 -0500

    Nustra historia en proceso

--- liebe_html/js/map.js
+++ liebe_html/js/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 		//Open street  Map
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+		var coord = [10.47388,-73.25315]; // <--- coordinates here
 
 		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
 
